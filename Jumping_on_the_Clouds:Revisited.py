c0a16a740660e9e0158caf0eb6337b5e212f8241
S GAYATHRI IYER <81139359+SGayathri05@users.noreply.github.com>
2023-07-29 14:46:36 +0530

    Create Jumping_on_the_Clouds:Revisited.py

---
+++ Jumping_on_the_Clouds:Revisited.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    energy=100
+    index=0
+    while True:
+        index=(index+k)%len(c)
+        if c[index]==0:
+            energy-=1
+        else:
+            energy-=3
+        if index==0:
+            break
+    return(energy) 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
